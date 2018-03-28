d0dbde6ccbcedc4b072a54897dab1416223882ea
hauphuong <luonghau91@gmail.com>
2018-03-28 22:18:35 +0700

    Commit

---
+++ STDShop.Data/Entities/Size.cs
@@ -0,0 +1,16 @@
+﻿using STDShop.Infrastruture.SharedKernel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace STDShop.Data.Entities
+{
+    [Table("Sizes")]
+    public class Size : DomainEntity<int>
+    {
+        [StringLength(250)]
+        public string Name
+        {
+            get; set;
+        }
+    }
+}
